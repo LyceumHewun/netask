3835852607a2377dcce2fc81accb408ba5073563
lyceum <hewun@lyceum.cc>
2022-01-27 01:01:43 +0800

    init

---
+++ src/main/java/cc/lyceum/netask/NetaskApplication.java
@@ -0,0 +1,13 @@
+package cc.lyceum.netask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetaskApplication.class, args);
+    }
+
+}
