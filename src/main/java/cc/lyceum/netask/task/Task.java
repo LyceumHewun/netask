e7cd32ad1813ce245520ba2d205d37878fdf0805
lyceum <hewun@lyceum.cc>
2022-01-31 16:56:53 +0800

    feat: log trace

--- src/main/java/cc/lyceum/netask/task/Task.java
+++ src/main/java/cc/lyceum/netask/task/Task.java
@@ -1,11 +1,13 @@
 package cc.lyceum.netask.task;
 
 import cc.lyceum.netask.core.Tree;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.Trigger;
 
 import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Lyceum
@@ -15,10 +17,12 @@ public class Task implements Runnable {
     private static final Logger LOG = LoggerFactory.getLogger(Task.class);
 
     private final String id;
+    @JsonIgnore
     private final Tree mainTree;
     private final Trigger trigger;
 
     private ScheduledFuture<?> future;
+    private final AtomicInteger count = new AtomicInteger(0);
 
     public Task(String id, Tree mainTree, Trigger trigger) {
         this.id = id;
@@ -30,7 +34,9 @@ public class Task implements Runnable {
     public void run() {
         try {
             mainTree.callNodeActionChain();
+            LOG.info("task [{}] completed {} times", getId(), count.incrementAndGet());
         } catch (Throwable e) {
+            e.printStackTrace();
             LOG.error("task [{}] error occurred: {}", getId(), e.toString());
         }
     }
